03b419e7ab9d4a86f78e8a49b58cf824d703935a
eun-hak <akfmzh1207@suwon.ac.kr>
2024-05-13 05:15:28 +0000

    fix: merge test

---
+++ src/components/shared/MainContainer.tsx
@@ -0,0 +1,8 @@
+import React, { ReactNode } from 'react';
+
+const MainContainer = ({ children }: { children: ReactNode }) => {
+  //
+  return <div className=" max-w-[393px]  mx-auto">{children}</div>;
+};
+
+export default MainContainer;
