4b2861ed47042d8280c9a4329c54a9179712ea75
eun-hak <akfmzh1207@suwon.ac.kr>
2024-07-04 14:43:39 +0000

    fix: lint fix

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "cypress";
-
+import { defineConfig } from 'cypress';
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  },
+    }
+  }
 });
